3877aa8cec56cda48289962bfdf1898fb5f0e294
Matthew Kitt <mk@mkitt.net>
2018-02-06 05:45:47 -0700

    Use https for assets

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? 'http://mkitt.net/hello' : '',
+  assetPrefix: isProd ? 'https://mkitt.net/hello' : '',
   exportPathMap() {
     return {
       '/': { page: '/' },
