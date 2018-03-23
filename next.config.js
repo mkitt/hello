76dd11a1f2b357625b3a0d049a6d3b9b315ce909
Matthew Kitt <mk@mkitt.net>
2018-03-23 17:43:10 -0600

    Cleanup and updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  // GitHub pages...
   assetPrefix: isProd ? 'https://mkitt.net/hello' : '',
   exportPathMap() {
     return {
